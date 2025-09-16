e8fcab4389d6896de84bf06138d91092ba62ae36
Piyush Kumar <145194744+piyrk@users.noreply.github.com>
2025-09-16 23:42:54 +0530

    Update CarRentalSystemApplication.java

--- car-rental-system-backend-master/car-rental-system-backend-master/src/main/java/com/carrentalsystem/CarRentalSystemApplication.java
+++ car-rental-system-backend-master/car-rental-system-backend-master/src/main/java/com/carrentalsystem/CarRentalSystemApplication.java
@@ -6,48 +6,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import com.carrentalsystem.entity.User;
 import com.carrentalsystem.service.UserService;
 import com.carrentalsystem.utility.Constants.ActiveStatus;
 import com.carrentalsystem.utility.Constants.UserRole;
 
 @SpringBootApplication
-public class CarRentalSystemApplication implements CommandLineRunner {
-	
-	private final Logger LOG = LoggerFactory.getLogger(CarRentalSystemApplication.class);
-	
-	@Autowired
-	private UserService userService;
+public class CarRentalSystemApplication extends SpringBootServletInitializer implements CommandLineRunner {
+    private final Logger LOG = LoggerFactory.getLogger(CarRentalSystemApplication.class);
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserService userService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarRentalSystemApplication.class, args);
-	}
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-		User admin = this.userService.getUserByEmailIdAndRoleAndStatus("demo.admin@demo.com",
-				UserRole.ROLE_ADMIN.value(), ActiveStatus.ACTIVE.value());
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalSystemApplication.class, args);
+    }
 
-		if (admin == null) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CarRentalSystemApplication.class);
+    }
 
-			LOG.info("Admin not found in system, so adding default admin");
-
-			User user = new User();
-			user.setEmailId("demo.admin@demo.com");
-			user.setPassword(passwordEncoder.encode("123456"));
-			user.setRole(UserRole.ROLE_ADMIN.value());
-			user.setStatus(ActiveStatus.ACTIVE.value());
-
-			this.userService.addUser(user);
-
-		}
-		
-	}
-
+    @Override
+    public void run(String... args) throws Exception {
+        User admin = this.userService.getUserByEmailIdAndRoleAndStatus("demo.admin@demo.com",
+                UserRole.ROLE_ADMIN.value(), ActiveStatus.ACTIVE.value());
+        if (admin == null) {
+            LOG.info("Admin not found in system, so adding default admin");
+            User user = new User();
+            user.setEmailId("demo.admin@demo.com");
+            user.setPassword(passwordEncoder.encode("123456"));
+            user.setRole(UserRole.ROLE_ADMIN.value());
+            user.setStatus(ActiveStatus.ACTIVE.value());
+            this.userService.addUser(user);
+        }
+    }
 }
